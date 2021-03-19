4e383ef374791bb14d556d8a615d05a6deacb35d
Jefferson Hinke <jefferson.hinke@noaa.gov>
2021-03-19 10:56:27 -0500

    updating help

--- R/photoR.R
+++ R/photoR.R
@@ -4,6 +4,9 @@
 #' Running the photoR app will spawn an interactive web page that allows the user to upload two data sets, correctly formatted and in a .csv format.
 #' The attendance data should have the following headers (case sensitive):SPLIT_YEAR, ROOKERY, COLONY, CAMERA,	SPP, NEST,	DATE, and	MAXN. Note that DATE can be specified in any way (default is for m/d/y), or as separate columns named DAY, MON, YR.
 #' The nest content data should have the following headers (case sensitive):SPLIT_YEAR,  ROOKERY,  SPP,  COLONY,  CAMERA,  NEST, DATE,  LAY,  MAXE,  HATCH,  MAXC, and  CRECHE. Note that DATE can be specified in anyway (default is for m/d/y), or as separate columns named DAY, MON, YR.
+#'
+#' The app will iteratively check the input data before processing. It first calls 'import_validation()' to address errors in the headers supplied in the data. Once error free, it then calls 'error_checking' to assess potential errors in the data. The user must fix any identified errors before processing can proceed.
+#'
 #' Protocols for data preparation are in progress, but available from the author upon request.
 #' @return A dataframe for download as a .csv file. The data are summarized  and formatted to fit specific CEMP Protocol data forms (www.ccamlr.org).
 #' @usage photoR()
